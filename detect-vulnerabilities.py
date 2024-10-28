5a880bfea360c3f1fac29c93fa566218ef76adca
Luís Vieira <lvieirapt@gmail.com>
2024-10-28 21:21:14 +0000

    added weight values to filename

--- detect-vulnerabilities.py
+++ detect-vulnerabilities.py
@@ -537,7 +537,8 @@ for hidden_dimension in hidden_dimension_options:
     model_vgg = VGGnet(in_channels=conv2dChannelParam).to(device)
 
     #Class weighting
-    weight = torch.tensor([1, 101000], dtype=torch.float, device=device)
+    weight_values = [1, 101000]
+    weight = torch.tensor(weight_values , dtype=torch.float, device=device)
     loss_func = nn.CrossEntropyLoss(weight=weight)
     #loss_func = nn.CrossEntropyLoss() # nn.NLLLoss() #nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
@@ -588,7 +589,7 @@ for hidden_dimension in hidden_dimension_options:
         stats_dict['epoch_losses'].append(epoch_loss)
         stats_dict['epoch_accuracy'].append(accuracy)
 
-    artifact_suffix = f"-{project}-{version}-{hidden_dimension}n-{normalization}e-{num_epochs}-us-{UNDERSAMPLING_STRAT}{UNDERSAMPLING_METHOD}"
+    artifact_suffix = f"-{project}-{version}-{hidden_dimension}n-{normalization}e-{num_epochs}-us-{UNDERSAMPLING_STRAT}{UNDERSAMPLING_METHOD}-w-{weight_values[0]}{weight_values[1]}"
     artifact_suffix += f"-sw{sample_weight_value}-size1-{type(model).__name__}-k{k_sortpooling}"
     artifact_suffix += f"-vgg-dr{dropout_rate}-c2d{conv2dChannelParam}"
 
